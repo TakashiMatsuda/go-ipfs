8ec48422281cbcc364c8a93048c5d1c74a7487ff
Takashi Matsuda <takashi.mtsd.dev@gmail.com>
2021-05-19 23:23:17 +0900

    wip rm command works

--- core/commands/swarm.go
+++ core/commands/swarm.go
@@ -63,63 +63,14 @@ const (
 )
 
 var swarmPeeringCmd = &cmds.Command{
-	// Options defines the flags accepted by the command. Flags on specified
-	// on parent commands are inherited by sub commands.
-	// Options []Option
-
-	// Arguments defines the positional arguments for the command. These
-	// arguments can be strings and/or files.
-	//
-	// The rules for valid arguments are as follows:
-	//
-	// 1. No required arguments may follow optional arguments.
-	// 2. There can be at most one STDIN argument.
-	// 3. There can be at most one variadic argument, and it must be last.
-	// Arguments []Argument
-
-	// Encoders encode results from Run (and/or PostRun) in the desired
-	// encoding.
-	//Encoders EncoderMap
-
-	Run: func(req *cmds.Request, res cmds.ResponseEmitter, env cmds.Environment) error {
-		fmt.Print("Hello, this is the test for peering\n.")
-		return nil
-	},
-
 	Helptext: cmds.HelpText{
 		Tagline:          "hogehoge",
 		ShortDescription: ``,
 	},
-
-	// Type describes the type of the output of the Command's Run Function.
-	// In precise terms, the value of Type is an instance of the return type of
-	// the Run Function.
-	//
-	// ie. If command Run returns &Block{}, then Command.Type == &Block{}
-	// Type: error{},
-
-	// Subcommands allow attaching sub commands to a command.
-	//
-	// Note: A command can specify both a Run function and Subcommands. If
-	// invoked with no arguments, or an argument that matches no
-	// sub commands, the Run function of the current command will be invoked.
-	//
-	// Take care when specifying both a Run function and Subcommands. A
-	// simple typo in a sub command will invoke the parent command and may
-	// end up returning a cryptic error to the user.
 	Subcommands: map[string]*cmds.Command{
 		"add": swarmPeeringAddCmd,
 		"rm":  swarmPeeringRmCmd,
 	},
-
-	// NoRemote denotes that a command cannot be executed in a remote environment
-	// NoRemote bool
-
-	// NoLocal denotes that a command cannot be executed in a local environment
-	// NoLocal bool
-
-	// Extra contains a set of other command-specific parameters
-	// Extra *Extra
 }
 
 var swarmPeeringAddCmd = &cmds.Command{
@@ -148,18 +99,13 @@ var swarmPeeringAddCmd = &cmds.Command{
 }
 
 var swarmPeeringRmCmd = &cmds.Command{
-	// Arguments defines the positional arguments for the command. These
-	// arguments can be strings and/or files.
-	//
-	// The rules for valid arguments are as follows:
-	//
-	// 1. No required arguments may follow optional arguments.
-	// 2. There can be at most one STDIN argument.
-	// 3. There can be at most one variadic argument, and it must be last.
+	Helptext: cmds.HelpText{},
 	Arguments: []cmds.Argument{
 		cmds.StringArg("ID", true, true, "IDs of peers to remove"),
 	},
-
+	Options: []cmds.Option{
+		cmds.BoolOption("save", "save to config."),
+	},
 	Run: func(req *cmds.Request, res cmds.ResponseEmitter, env cmds.Environment) error {
 		args := req.Arguments
 		node, err := cmdenv.GetNode(env)
@@ -170,6 +116,9 @@ var swarmPeeringRmCmd = &cmds.Command{
 			node.Peering.RemovePeer(peer.ID(v))
 		}
 		return nil
+	},
+	Encoders: cmds.EncoderMap{
+		cmds.Text: cmds.MakeTypedEncoder(stringListEncoder),
 	},
 }
 
